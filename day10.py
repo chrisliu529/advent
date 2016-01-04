15faed7a84b61e55ca5743fcbbf66d7badbfb1f6
Chris Liu <guohong.liu@gmail.com>
2016-01-04 19:38:53 +0800

    day10 half

---
+++ day10.py
@@ -0,0 +1,28 @@
+def do_trans(s):
+    i = 0
+    prev = s[i]
+    ns = ''
+    cnt = 0
+    ch = prev
+    while i < len(s):
+        while i < len(s) and ch == prev:
+            cnt += 1
+            i += 1
+            if i < len(s):
+                ch = s[i]            
+        ns += str(cnt) + prev
+        if i == len(s):
+            return ns
+        prev = ch
+        cnt = 0
+    return ns
+
+def trans(s, n):
+    for i in range(n):
+        s = do_trans(s)
+    return s
+
+def main():
+    print len(trans('3113322113', 40))
+
+main()
